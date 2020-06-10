bb2adf8c4d5c71dcc153fb235cede1a1966a9a0b
parideis <denispariskks@yahoo.de>
2020-06-10 12:08:38 +0200

    Init Stencil

---
+++ src/components/hallo-welt/hallo-welt.tsx
@@ -0,0 +1,17 @@
+import { Component, ComponentInterface, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'hallo-welt',
+  shadow: true,
+})
+export class HalloWelt implements ComponentInterface {
+
+  render() {
+    return (
+      <Host>Test
+        <slot></slot>
+      </Host>
+    );
+  }
+
+}
